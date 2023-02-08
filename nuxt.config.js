e039e9e180404ca91477cc984f4c4112a922060d
MoonJongHyeon1095 <109948801+MoonJongHyeon1095@users.noreply.github.com>
2023-02-08 10:18:15 +0900

    no message

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,52 +1,48 @@
+import { server } from "websocket";
+
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Qterview',
+    title: "Qterview",
     htmlAttrs: {
-      lang: 'en'
+      lang: "en",
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      { name: "format-detection", content: "telephone=no" },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ["@/assets/global.scss", "@/assets/question.scss"
-  ],
+  css: ["@/assets/global.scss", "@/assets/question.scss"],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-    '~/plugins/api'
-  ],
+  plugins: ["~/plugins/api"],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-  ],
+  buildModules: [],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
-    '@nuxtjs/style-resources',
-    '@nuxtjs/moment',
-    'nuxt-socket-io',
+    "@nuxtjs/axios",
+    "@nuxtjs/style-resources",
+    "@nuxtjs/moment",
+    "nuxt-socket-io",
   ],
   io: {
     // module options
     sockets: [
       {
-        name: 'alarm',
+        name: "alarm",
         // default: true,
-        url: 'http://localhost:3001',
+        url: "http://localhost:3001",
       },
     ],
   },
@@ -54,15 +50,15 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-     baseURL: '/',
+    baseURL: "/",
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  },
-
+  build: {},
+  ssr: false,
+  target: server,
   server: {
     port: 8080,
-    host: '0.0.0.0'
-  }
-}
+    host: "0.0.0.0",
+  },
+};
